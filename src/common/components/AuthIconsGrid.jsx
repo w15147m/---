56aad4684689c64ff72a92811ca3501d88b4bd91
waseem abbas <waseemofficee@gamil.com>
2026-02-15 20:21:22 +0500

    refactor: move shared UI components (Toast, AuthLayout, AuthIconsGrid, AlertModal) to common/components to align with project structure.

--- src/common/components/AuthIconsGrid.jsx
+++ src/common/components/AuthIconsGrid.jsx
@@ -1,32 +1,32 @@
 import React from 'react';
 import { View, Image, Dimensions, StyleSheet } from 'react-native';
 
-import { useTheme } from '../context/ThemeContext';
+import { useTheme } from '../../context/ThemeContext';
 
 const { height } = Dimensions.get('window');
 
 // Requiring icons with cleaned filenames (no spaces)
 const icons = [
-  require('../assets/3D-Icon/Rocket-small.png'),
-  require('../assets/3D-Icon/Calender-small.png'),
-  require('../assets/3D-Icon/Clock-small.png'),
-  require('../assets/3D-Icon/Target-small.png'),
-  require('../assets/3D-Icon/Brief_case_bag-small.png'),
-  require('../assets/3D-Icon/Credit_Card-small.png'),
-  require('../assets/3D-Icon/Folder-small.png'),
-  require('../assets/3D-Icon/Magnifying_Glass-small.png'),
-  require('../assets/3D-Icon/Smartphone-small.png'),
-  require('../assets/3D-Icon/Safe_Box-small.png'),
-  require('../assets/3D-Icon/Coin_Bag-small.png'),
-  require('../assets/3D-Icon/Desktop_computer_graph-small.png'),
-  require('../assets/3D-Icon/Bill_Printers-small.png'),
-  require('../assets/3D-Icon/Draf_Note-small.png'),
-  require('../assets/3D-Icon/Mail_Bill_Payment-small.png'),
-  require('../assets/3D-Icon/Money_Coin-small.png'),
-  require('../assets/3D-Icon/Note_Calculator-small.png'),
-  require('../assets/3D-Icon/Projector-small.png'),
-  require('../assets/3D-Icon/Rolls_of_Paper-small.png'),
-  require('../assets/3D-Icon/Speaker-small.png'),
+  require('../../assets/3D-Icon/Rocket-small.png'),
+  require('../../assets/3D-Icon/Calender-small.png'),
+  require('../../assets/3D-Icon/Clock-small.png'),
+  require('../../assets/3D-Icon/Target-small.png'),
+  require('../../assets/3D-Icon/Brief_case_bag-small.png'),
+  require('../../assets/3D-Icon/Credit_Card-small.png'),
+  require('../../assets/3D-Icon/Folder-small.png'),
+  require('../../assets/3D-Icon/Magnifying_Glass-small.png'),
+  require('../../assets/3D-Icon/Smartphone-small.png'),
+  require('../../assets/3D-Icon/Safe_Box-small.png'),
+  require('../../assets/3D-Icon/Coin_Bag-small.png'),
+  require('../../assets/3D-Icon/Desktop_computer_graph-small.png'),
+  require('../../assets/3D-Icon/Bill_Printers-small.png'),
+  require('../../assets/3D-Icon/Draf_Note-small.png'),
+  require('../../assets/3D-Icon/Mail_Bill_Payment-small.png'),
+  require('../../assets/3D-Icon/Money_Coin-small.png'),
+  require('../../assets/3D-Icon/Note_Calculator-small.png'),
+  require('../../assets/3D-Icon/Projector-small.png'),
+  require('../../assets/3D-Icon/Rolls_of_Paper-small.png'),
+  require('../../assets/3D-Icon/Speaker-small.png'),
 ];
 
 // Aesthetic background colors from the reference
